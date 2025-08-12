0458f00f1586e5f3cb0632c16ca3afbdbbeb674d
atique0x <satique06@gmail.com>
2025-08-12 15:09:59 +0600

    Web History

--- docs/Recap Javascript/Important Topics/17_web_history/script.js
+++ docs/Recap Javascript/Important Topics/17_web_history/script.js
@@ -1,2 +1,22 @@
 document.getElementById("history-length").innerText = history.length;
 
+function pushStateExample() {
+  history.pushState(
+    { page: "home" },
+    "",
+    "http://127.0.0.1:5500/docs/Recap%20Javascript/Important%20Topics/17_web_history/about.html"
+  );
+}
+
+function replaceStateExample() {
+  history.replaceState(
+    { page: "home" },
+    "",
+    "http://127.0.0.1:5500/docs/Recap%20Javascript/Important%20Topics/17_web_history/index.html"
+  );
+}
+
+window.addEventListener("popstate", (event) => {
+  console.log(event);
+  console.log(event.state);
+});
